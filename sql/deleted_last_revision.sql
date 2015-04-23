d876bcd86d6d59dbd69f0863e539c4b3a58dd090
Aaron Halfaker <aaron.halfaker@gmail.com>
2015-04-23 14:11:05 +0000

    Remamed sql file.

--- sql/deleted_last_revision.sql
+++ sql/deleted_last_revision.sql
@@ -3,6 +3,7 @@ SELECT
   ar_namespace AS page_namespace,
   ar_title AS page_title,
   MAX(ar_rev_id) AS last_rev_id,
-  MAX(ar_timestamp) AS last_timestamp
+  MAX(ar_timestamp) AS last_timestamp,
+  True AS archived
 FROM archive
 GROUP BY 1,2,3;
