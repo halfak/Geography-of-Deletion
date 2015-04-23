9ec8e0507543cabcded7e6938feba6dedc5c4a3b
Aaron Halfaker <aaron.halfaker@gmail.com>
2015-04-23 13:51:28 +0000

    Init with SQL

---
+++ sql/deleted_last_revisions.sql
@@ -0,0 +1,8 @@
+SELECT
+  ar_page_id AS page_id,
+  ar_namespace AS page_namespace,
+  ar_title AS page_title,
+  MAX(ar_rev_id) AS last_rev_id,
+  MAX(ar_timestamp) AS last_timestamp
+FROM archive
+GROUP BY 1,2,3;
