6e5999a03c50d1d8dd07c5caf9748676c15f751f
Aaron Halfaker <aaron.halfaker@gmail.com>
2015-04-23 14:15:19 +0000

    Adds columns to query.

--- sql/deleted_last_revision.sql
+++ sql/deleted_last_revision.sql
@@ -4,6 +4,10 @@ SELECT
   ar_title AS page_title,
   MAX(ar_rev_id) AS last_rev_id,
   MAX(ar_timestamp) AS last_timestamp,
+  MIN(ar_rev_id) AS first_rev_id,
+  MIN(ar_timestamp) AS first_timestamp,
+  COUNT(*) AS revisions,
   True AS archived
 FROM archive
 GROUP BY 1,2,3;
+
